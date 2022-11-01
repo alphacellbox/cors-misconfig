3afda6ba4b72c910c2a8ae24f978f8a9ca454307
cellbox <mo.khedmat1998@gmail.com>
2022-11-01 18:39:19 +0330

    delete propertise

--- src/main/java/cellboxowasp/corsmisconfig/Controller.java
+++ src/main/java/cellboxowasp/corsmisconfig/Controller.java
@@ -7,7 +7,7 @@ public class Controller {
 
    // @RequestMapping(value = {"/{a}"}, method = RequestMethod.GET)
 
-    @CrossOrigin(value ="http://127.0.0.1:5500")
+    @CrossOrigin(value ="http://127.0.0.2:550")
     @RequestMapping("/")
      public String all(){
         System.out.println("aaaaaaaaaaaaa");
