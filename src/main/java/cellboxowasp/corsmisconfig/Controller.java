81772fc6673182c6f64415323ed239ace616aab5
Cellbox <mo.khedmat1998@gmial.com>
2022-11-04 23:27:06 +0330

    added ip and time

--- src/main/java/cellboxowasp/corsmisconfig/Controller.java
+++ src/main/java/cellboxowasp/corsmisconfig/Controller.java
@@ -1,17 +1,31 @@
 package cellboxowasp.corsmisconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @RestController
 public class Controller {
-
+    @Autowired
+    private RequestServiceImpl requestService;
    // @RequestMapping(value = {"/{a}"}, method = RequestMethod.GET)
 
     @CrossOrigin(value ="*")
     @RequestMapping("/")
-     public String all(){
-        System.out.println("aaaaaaaaaaaaa");
-        return "amir chombe";
+     public String all(HttpServletRequest httpServletRequest){
+        System.out.println("///////////////////////");
+        OffsetDateTime offsetdatetime
+                = OffsetDateTime.now();
+        System.out.println(">>>>> ip = "+requestService.getClientIp(httpServletRequest));
+        System.out.println(offsetdatetime);
+        System.out.println("*********************");
+        return "sabzi polo ba mahi";
     }
 }
 
